8260f4cb213a6ffa38530608f9295a1d3926125e
beduffy <benfduffy@gmail.com>
2023-10-19 16:17:20 +0200

    Added I to PI controller, not sure if it is helping yet. Better printing, calculated pure face detection at 16-22 FPS varying

--- servo_track_face.py
+++ servo_track_face.py
@@ -33,12 +33,14 @@ def camera():
     # TIME_MIN_SINCE_LAST_COMMAND = 1
     # TIME_MIN_SINCE_LAST_COMMAND = 0.3
     time_since_last_servo_command_sent = time.time()
+    error_integral = 0.0
 
     ret, frame = cam.read()
     h, w = frame.shape[:2]
     centerface = CenterFace()
 
     while True:
+        start_loop_time = time.time()
         ret, frame = cam.read()
 
         dets, lms = centerface(frame, h, w, threshold=0.35)
@@ -54,7 +56,8 @@ def camera():
         for lm in lms:
             for i in range(0, 5):
                 cv2.circle(frame, (int(lm[i * 2]), int(lm[i * 2 + 1])), 2, (0, 0, 255), -1)
-        
+
+        # if 5 == 4:
         if chosen_center_pix_pos:
             cv2.circle(frame, chosen_center_pix_pos, 10, (0, 0, 255))
 
@@ -87,10 +90,19 @@ def camera():
                 # P_const = 0.28125
                 # P_const = 0.15
                 P_const = 0.03
-                amount_to_rotate_by = horizontal_distance_to_center_x_bbox * P_const
+                # I_const = 0.01
+                I_const = 0.0001
+                error = horizontal_distance_to_center_x_bbox  # is this the error? TODO. I always struggled with PID units being different e.g. temperature vs temperature. Speed vs position.
+                error_integral += error * (TIME_MIN_SINCE_LAST_COMMAND)  # TODO it might not be TIME_MIN_SINCE_LAST_COMMAND
+                # TODO understand I better
+                
+                # amount_to_rotate_by = horizontal_distance_to_center_x_bbox * P_const  # just P
+                amount_to_rotate_by = P_const * error + I_const * error_integral
                 curr_servo_val += amount_to_rotate_by
-                print('horizontal distance: ', horizontal_distance_to_center_x_bbox)
-                print('amount_to_rotate_by:', amount_to_rotate_by)
+                print('horizontal distance: {}. error_integral * I const: {:.3f}. amount_to_rotate_by: {:.3f}. Flask angle: {:.3f}'.format(
+                    horizontal_distance_to_center_x_bbox, I_const * error_integral, amount_to_rotate_by, curr_servo_val))
+                # print('error_integral:', error_integral)
+                # print('amount_to_rotate_by:', amount_to_rotate_by)
 
                 if curr_servo_val < 0:
                     curr_servo_val = 0
@@ -98,13 +110,17 @@ def camera():
                     curr_servo_val = 180
 
 
-                post_servo_value(curr_servo_val)
+                post_servo_value(curr_servo_val)  # TODO swap to grequests? async
+                # TODO multi threading between servo control and webcam?
 
         cv2.imshow('out', frame)
 
         # Press Q on keyboard to stop recording
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        # time_taken = time.time() - start_loop_time
+        # print('Time taken: {:.3f}. FPS: {:.3f}'.format(time_taken, 1 / time_taken))
 
     cam.release()
 
