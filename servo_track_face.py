287ccab2d2dc2c4a9d108c738ce7b7b5b77b09b4
beduffy <benfduffy@gmail.com>
2023-10-24 16:01:52 +0200

    Added PID class to make it easy to do hori and verti together. Found vertical distance. much cleaner. Moved TODOs down and removed some

--- servo_track_face.py
+++ servo_track_face.py
@@ -9,24 +9,67 @@ from centerface import CenterFace
 from person_detection_from_cctv_video.send_servo_val_requests import post_servo_value
 
 
+class PID_pixels_to_servo_angles():
+    def __init__(self, error_integral_rate) -> None:
+         
+        # this PID converts from horizontal pixel distances to degree rotates to add to servo val
+        # Therefore the max is 640 pixels -> 180 degrees
+        # 640 / 180 = 3.5 P
+        # So if 100 pixels away: 3.5 x 100
+        # or 180 / 640 = 0.28125
+        # So if 100 pixels away: 0.28125 x 100 = 28.125 degrees to change... 
+        # TODO could also calculate how many degrees theoretically with a target 3 metres away? hmm, manual tuning
+        # if 10 pixels away: 0.281125 x 10 = 2.8 degrees...
+
+        # TODO easily swap from PID to just left right
+        # TODO easily swap from face track to colour track to person track?
+        
+        # self.P_const = 0.28125
+        # self.P_const = 0.15
+        self.P_const = 0.03  # very good
+        self.P_const = 0.01  # gets to ~2 horizontal distance
+
+        # self.I_const = 0.01
+        self.I_const = 0.0001
+
+        self.error_integral = 0.0
+        self.error_integral_rate = error_integral_rate
+
+    def get_amount_to_rotate_by(self, pixel_dist):
+        # TODO crazy idea of big P if horizontal distance over 100
+        if pixel_dist > 50:
+            P_const = 0.15
+        else:
+            P_const = 0.01
+        
+        self.error = pixel_dist  # is this the error? TODO. I always struggled with PID units being different e.g. temperature vs temperature. Speed vs position.
+        self.error_integral += self.error * (self.error_integral_rate)  # TODO it might not be TIME_MIN_SINCE_LAST_COMMAND
+        # TODO understand I better
+        
+        # amount_to_rotate_by = horizontal_distance_to_center_x_bbox * P_const  # just P
+        amount_to_rotate_by = P_const * self.error
+        # amount_to_rotate_by = P_const * error + I_const * error_integral
+        # print('error_integral:', error_integral)
+        # print('amount_to_rotate_by:', amount_to_rotate_by)
+
+        return amount_to_rotate_by
 
 
 
 def camera():
-    # TODO below into function
+    # TODO below camera stuff into function
     width = 640
-    im_half_width = int(width / 2)
     height = 360
-    # cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    # cam = cv2.VideoCapture(0)
-    # cam = cv2.VideoCapture(1)
+    img_half_width = int(width / 2)
+    img_half_height = int(height / 2)
+    # cam = cv2.VideoCapture(0,cv2.CAP_DSHOW) # TODO what is dshow?
     cam = cv2.VideoCapture(4)
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     curr_servo_val = 90
     post_servo_value(curr_servo_val)
-    # TODO soon
+    # TODO soon or not?
     # servo_control_frame_rate = 10
 
     TIME_MIN_SINCE_LAST_COMMAND = 0.1
@@ -34,8 +77,12 @@ def camera():
     # TIME_MIN_SINCE_LAST_COMMAND = 1
     # TIME_MIN_SINCE_LAST_COMMAND = 0.3
     time_since_last_servo_command_sent = time.time()
-    error_integral = 0.0
-    remembered_chosen_center_pix_pos = None  # so we keep going towards that direction if human is really fast
+    
+
+    pid_horizontal = PID_pixels_to_servo_angles(TIME_MIN_SINCE_LAST_COMMAND)
+    pid_vertical = PID_pixels_to_servo_angles(TIME_MIN_SINCE_LAST_COMMAND)
+
+    remembered_chosen_center_pix_pos = None  # so we keep going towards that direction if human is really fast. hmm flaws 
 
     ret, frame = cam.read()
     h, w = frame.shape[:2]
@@ -67,16 +114,18 @@ def camera():
             #     chosen_center_pix_pos = remembered_chosen_center_pix_pos  # TODO sometimes works, sometimes does not
             cv2.circle(frame, chosen_center_pix_pos, 10, (0, 0, 255))
 
-            horizontal_distance_to_center_x_bbox = im_half_width - chosen_center_pix_pos[0]
-            # horizontal_distance_to_center_x_bbox = chosen_center_pix_pos[0] - im_half_width
+            horizontal_distance_to_center_x_bbox = img_half_width - chosen_center_pix_pos[0]
+            vertical_distance_to_center_y_bbox = img_half_height - chosen_center_pix_pos[1]  # TODO flip or?
 
             cv2.line(frame, (chosen_center_pix_pos[0], chosen_center_pix_pos[1]), 
-                        (im_half_width, chosen_center_pix_pos[1]), (0, 255, 0), thickness=3)
+                     (img_half_width, chosen_center_pix_pos[1]), (0, 255, 0), thickness=2)
+            cv2.line(frame, (chosen_center_pix_pos[0], chosen_center_pix_pos[1]), 
+                     (chosen_center_pix_pos[0], img_half_height), (255, 255, 0), thickness=2)
 
             if time.time() - time_since_last_servo_command_sent > TIME_MIN_SINCE_LAST_COMMAND:
                 time_since_last_servo_command_sent = time.time()
                 
-                # amount_to_rotate_by = 0.1  # this can be PID'd TODO
+                # amount_to_rotate_by = 0.1  # instead of PID, stupid but works
                 # if horizontal_distance_to_center_x_bbox > 0:
                 #     print('distance positive, object to left')
                 #     curr_servo_val += amount_to_rotate_by
@@ -84,38 +133,12 @@ def camera():
                 #     print('distance negative, object pospost_servo_value(curr_servo_val)t_servo_value(curr_servo_val)to right')
                 #     curr_servo_val -= amount_to_rotate_by
 
-                # this PID converts from horizontal pixel distances to degree rotates to add to servo val
-                # Therefore the max is 640 pixels -> 180 degrees
-                # 640 / 180 = 3.5 P
-                # So if 100 pixels away: 3.5 x 100
-                # or 180 / 640 = 0.28125
-                # So if 100 pixels away: 0.28125 x 100 = 28.125 degrees to change... 
-                # TODO could also calculate how many degrees theoretically with a target 3 metres away? hmm, manual tuning
-                # if 10 pixels away: 0.281125 x 10 = 2.8 degrees...
+                amount_to_rotate_by = pid_horizontal.get_amount_to_rotate_by(horizontal_distance_to_center_x_bbox)
 
-                # P_const = 0.28125
-                # P_const = 0.15
-                P_const = 0.03  # very good
-                P_const = 0.01  # gets to ~2 horizontal distance
-                # TODO crazy idea of big P if horizontal distance over 100
-                if horizontal_distance_to_center_x_bbox > 50:
-                    P_const = 0.15
-                else:
-                    P_const = 0.01
-                # I_const = 0.01
-                I_const = 0.0001
-                error = horizontal_distance_to_center_x_bbox  # is this the error? TODO. I always struggled with PID units being different e.g. temperature vs temperature. Speed vs position.
-                error_integral += error * (TIME_MIN_SINCE_LAST_COMMAND)  # TODO it might not be TIME_MIN_SINCE_LAST_COMMAND
-                # TODO understand I better
-                
-                # amount_to_rotate_by = horizontal_distance_to_center_x_bbox * P_const  # just P
-                amount_to_rotate_by = P_const * error
-                # amount_to_rotate_by = P_const * error + I_const * error_integral
                 curr_servo_val += amount_to_rotate_by
-                print('horizontal distance: {}. error_integral * I const: {:.3f}. amount_to_rotate_by: {:.3f}. Flask angle: {:.3f}'.format(
-                    horizontal_distance_to_center_x_bbox, I_const * error_integral, amount_to_rotate_by, curr_servo_val))
-                # print('error_integral:', error_integral)
-                # print('amount_to_rotate_by:', amount_to_rotate_by)
+                print('horizontal dist: {}. vert dist: {}. error_integral * I const: {:.3f}. amount_to_rotate_by: {:.3f}. Flask angle: {:.3f}'.format(
+                    horizontal_distance_to_center_x_bbox, vertical_distance_to_center_y_bbox, 
+                    pid_horizontal.I_const * pid_horizontal.error_integral, amount_to_rotate_by, curr_servo_val))
 
                 if curr_servo_val < 0:
                     curr_servo_val = 0
@@ -123,8 +146,7 @@ def camera():
                     curr_servo_val = 180
 
 
-                post_servo_value(curr_servo_val)  # TODO swap to grequests? async
-                # TODO multi threading between servo control and webcam?
+                post_servo_value(curr_servo_val)  
 
         cv2.imshow('out', frame)
 
@@ -132,11 +154,14 @@ def camera():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        # Reporting FPS, varies even without servo control, between 16-21
         # time_taken = time.time() - start_loop_time
         # print('Time taken: {:.3f}. FPS: {:.3f}'.format(time_taken, 1 / time_taken))
 
     cam.release()
 
+# TODO swap to grequests? async
+# TODO multi threading between servo control and webcam?
 # TODO python matplotlib plots at the same time?
 # TODO full 360 degrees. TODO encoders and DC motors? or brushless or? or two servos on same axis to cover all 360 degrees? that's worth a video by itself, hmmm
 # TODO servo is only moving when i tell it to move 3 degrees?!??! get better servo or do what I said above
@@ -144,6 +169,7 @@ def camera():
 # TODO tilt
 # TODO mechanical structure without books
 # TODO if person leaves camera, then do a sentry look around AND/OR keep going that direction
+
 
 if __name__ == '__main__':
     camera()
