13d599ffa4ffa2c696c2ee256efa338537da3427
beduffy <benfduffy@gmail.com>
2023-10-24 16:02:04 +0200

    Sent servo_index in send_servo_val_requests

--- person_detection_from_cctv_video/send_servo_val_requests.py
+++ person_detection_from_cctv_video/send_servo_val_requests.py
@@ -7,22 +7,23 @@ import sys
 
 print(sys.argv)
 
-
-def post_servo_value(servo_value):
-    # curl 192.168.178.125:8080/control_servo -H "Content-Type: application/json" -d '{"servo_value": "0"}'
+# TODO move outside of this folder
+# curl 192.168.178.125:8080/control_servo -H "Content-Type: application/json" -d '{"servo_value": "0"}'
+def post_servo_value(servo_value, servo_idx=0):
     # IP = '192.168.178.125'
     IP = '192.168.178.36'
 
     url = 'http://{}:8080/control_servo'.format(IP)
-    myobj = {'servo_value': '{}'.format(servo_value)}
+    data_to_send = {'servo_value': '{}'.format(servo_value), 
+                    'servo_index': servo_idx}
 
     # print(url)
-    # x = requests.post(url, json = myobj, headers={"Content-Type": "application/json"})
     # x = requests.post(url, data = json.dumps(myobj), headers={"Content-Type": "application/json"})
-    rs = (grequests.post(url, data = json.dumps(myobj), headers={"Content-Type": "application/json"}), )
+    rs = (grequests.post(url, data = json.dumps(data_to_send), headers={"Content-Type": "application/json"}), )
     grequests.map(rs)
     # print('Sent {:.3f} angle to flask'.format(servo_value))
     # print(x.text)
 
 if __name__ == '__main__':
-    post_servo_value(sys.argv[1])
+    # post_servo_value(sys.argv[1])
+    post_servo_value(sys.argv[1], sys.argv[2])
