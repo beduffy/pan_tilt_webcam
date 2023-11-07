d05b8944194c158d7b71775460076cbf9bb65f68
beduffy <benfduffy@gmail.com>
2023-11-07 14:36:41 +0100

    Got serial_control_stepper.py working in loop and tried random angles and pdb

--- serial_control_stepper.py
+++ serial_control_stepper.py
@@ -1,23 +1,32 @@
 import serial
 
+import numpy as np
+
 s = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
 # s = serial.Serial('/dev/ttyACM0', 9600, bytesize=8, timeout = 1)
 
 angle = 50.0
-
-# res = s.write('10.0')
-
 cmd = '{}\n'.format(angle)
 
 res = s.write(cmd.encode())
 s.flush()
 
-# TODO maybe I just need to push a few liens across first to get the serial connection started? or sleep?
-for i in range(5):
+# chosen_angles = [0, 100, 100, -100, -100]
+chosen_angles = [0, -100, 100, 100, -100, -100]
 
+# TODO maybe I just need to push a few lines across first to get the serial connection started? or sleep?
+for i in range(5):
 # while True:
-    r = s.readline()
-    print(r)
-    res = s.write('100.0\n'.encode())
+
+    # angle = np.random.choice([0, 50, 100, 20, 30])
+    angle = chosen_angles[i]
+    cmd = '{}\n'.format(angle)
+    
+    res = s.write(cmd.encode())
     s.flush()
+    print('Sent: ', cmd)
+
+    r = s.readline()
+    print('Got serial line: ', r)
+    import pdb;pdb.set_trace()
     # print(res)
