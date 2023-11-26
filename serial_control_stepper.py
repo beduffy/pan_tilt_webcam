af425f286b686cae8b5688b1f202c4607cb7d988
beduffy <benfduffy@gmail.com>
2023-11-26 19:26:18 +0100

    Made stepper wiggle fast and tested limit of speed and serial connection, seems fine

--- serial_control_stepper.py
+++ serial_control_stepper.py
@@ -1,8 +1,10 @@
 import serial
+import time
 
 import numpy as np
 
-s = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+# s = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+s = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
 # s = serial.Serial('/dev/ttyACM0', 9600, bytesize=8, timeout = 1)
 
 angle = 50.0
@@ -12,15 +14,16 @@ res = s.write(cmd.encode())
 s.flush()
 
 # chosen_angles = [0, 100, 100, -100, -100]
-chosen_angles = [0, -100, 100, 100, -100, -100]
+chosen_angles = [0, 30, -30, 30, -30, 30, -30, 30, -30]
 
 # TODO maybe I just need to push a few lines across first to get the serial connection started? or sleep?
-for i in range(5):
+# for i in range(5):
+for i in range(9):
 # while True:
 
     # angle = np.random.choice([0, 50, 100, 20, 30])
     angle = chosen_angles[i]
-    cmd = '{}\n'.format(angle)
+    cmd = 'a{}\n'.format(angle)
     
     res = s.write(cmd.encode())
     s.flush()
@@ -28,5 +31,7 @@ for i in range(5):
 
     r = s.readline()
     print('Got serial line: ', r)
-    import pdb;pdb.set_trace()
+    
+    time.sleep(0.1)
+    # import pdb;pdb.set_trace()
     # print(res)
