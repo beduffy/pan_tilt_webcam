9ba57d3084273f0c982cdb340aa8e99cabcbe855
beduffy <benfduffy@gmail.com>
2024-03-25 23:17:25 +0100

    Created better PID class

--- track_face_control_create_3_ros2.py
+++ track_face_control_create_3_ros2.py
@@ -1,7 +1,9 @@
+import sys
+import time
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import time
 import cv2
 from centerface import CenterFace
 
@@ -11,14 +13,15 @@ class FaceTracker(Node):
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
         self.timer_period = 0.1  # seconds
         self.timer = self.create_timer(self.timer_period, self.track_face)
-        self.cam = cv2.VideoCapture(2)
+        self.cam = cv2.VideoCapture(4)
+        # self.cam = cv2.VideoCapture(1, cv2.CAP_V4L)
         self.width = 640
-        self.img_half_width = self.width // 2
         self.height = 360
+        self.img_half_width = self.width // 2
         self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         self.centerface = CenterFace()
-        self.pid_horizontal = PID_pixels_to_servo_angles(self.timer_period, use_changing_PID=True)
+        self.pid_horizontal = PIDController(kp=0.001, ki=0.0001, kd=0.0001)
 
     def track_face(self):
         ret, frame = self.cam.read()
@@ -34,7 +37,7 @@ class FaceTracker(Node):
             boxes, score = det[:4], det[4]
             cv2.rectangle(frame, (int(boxes[0]), int(boxes[1])), (int(boxes[2]), int(boxes[3])), (2, 255, 0), 1)
             chosen_center_pix_pos = (int(round((boxes[2] + boxes[0]) / 2)), int(round((boxes[3] + boxes[1]) / 2)))
-            remembered_chosen_center_pix_pos = chosen_center_pix_pos   # TODO do I need to remember the angle?
+            # remembered_chosen_center_pix_pos = chosen_center_pix_pos   # TODO do I need to remember the angle?
             cv2.circle(frame, (int(boxes[0]), int(boxes[1])), 10, (0, 255, 255))
             cv2.circle(frame, (int(boxes[2]), int(boxes[3])), 10, (255, 255, 255))
 
@@ -50,43 +53,49 @@ class FaceTracker(Node):
             cv2.line(frame, (chosen_center_pix_pos[0], chosen_center_pix_pos[1]), 
                         (self.img_half_width, chosen_center_pix_pos[1]), (0, 255, 0), thickness=2)
 
-            for det in dets:
-                boxes = det[:4]
-                chosen_center_pix_pos = (int(round((boxes[2] + boxes[0]) / 2)), int(round((boxes[3] + boxes[1]) / 2)))
-                horizontal_distance_to_center_x_bbox = int(self.width / 2) - chosen_center_pix_pos[0]
-                amount_to_rotate_by = self.pid_horizontal.get_amount_to_rotate_by(horizontal_distance_to_center_x_bbox)
-                self.send_twist_command(amount_to_rotate_by)
-                break  # Only track the first face detected for simplicity
+            horizontal_distance_to_center_x_bbox = self.img_half_width - chosen_center_pix_pos[0]
+            amount_to_rotate_by = self.pid_horizontal.update(horizontal_distance_to_center_x_bbox)
+            self.send_twist_command(amount_to_rotate_by)
+            # Only track the first face detected for simplicity
+        else:
+            print('No faces detected')
 
-            cv2.imshow('out', frame)
+        cv2.imshow('out', frame)
 
-            # Press Q on keyboard to stop recording
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                sys.exit()
+        # Press Q on keyboard to stop recording
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            sys.exit()
             
 
     def send_twist_command(self, amount_to_rotate_by):
         twist_msg = Twist()
         twist_msg.linear.x = 0.0
         twist_msg.angular.z = amount_to_rotate_by
-        # self.publisher_.publish(twist_msg)
+        self.publisher_.publish(twist_msg)
         self.get_logger().info(f'Sending twist command: linear.x={twist_msg.linear.x}, angular.z={twist_msg.angular.z}')
 
-class PID_pixels_to_servo_angles():
-    def __init__(self, error_integral_rate, use_changing_PID=False) -> None:
-        self.P_const = 0.01
-        self.I_const = 0.0001
-        self.error_integral = 0.0
-        self.error_integral_rate = error_integral_rate
-        self.use_changing_PID = use_changing_PID
 
-    def get_amount_to_rotate_by(self, pixel_dist):
-        if self.use_changing_PID:
-            P_const = 0.15 if pixel_dist > 50 else 0.01
-        else:
-            P_const = 0.01
-        self.error_integral += pixel_dist * self.error_integral_rate
-        return P_const * pixel_dist
+class PIDController():
+    def __init__(self, kp, ki, kd, set_point=0.0) -> None:
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.set_point = set_point
+        self.error_integral = 0.0
+        self.last_error = 0.0
+        self.error_derivative = 0.0
+
+    def update(self, pixel_horizontal_error):
+        self.error_integral += pixel_horizontal_error
+        self.error_derivative = pixel_horizontal_error - self.last_error
+        self.last_error = pixel_horizontal_error
+
+        p_term = self.kp * pixel_horizontal_error
+        i_term = self.ki * self.error_integral
+        d_term = self.kd * self.error_derivative
+
+        return p_term + i_term + d_term
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -95,6 +104,7 @@ def main(args=None):
     face_tracker.cam.release()
     face_tracker.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
